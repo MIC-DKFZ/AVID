3ea1c07d7914e69202c9394124824dc764451c37
Ralf Floca <r.floca@dkfz-heidelberg.de>
2017-03-28 17:09:52 +0200

    Fixed T22632 Added batch action, added first simple test, fixed some errors in the action.
    Signed-off-by: Ralf Floca <r.floca@dkfz-heidelberg.de>

--- avid/actions/plmRTSSMap.py
+++ avid/actions/plmRTSSMap.py
@@ -13,6 +13,7 @@
 
 import os
 import logging
+from shutil import copyfile
 
 import avid.common.artefact.defaultProps as artefactProps
 import avid.common.artefact as artefactHelper
@@ -24,12 +25,11 @@ from cliActionBase import CLIActionBase
 from avid.linkers import CaseLinker
 from avid.selectors import TypeSelector
 from simpleScheduler import SimpleScheduler
-from avid.externals.plastimatch import parseDiceResult, FORMAT_VALUE_PLM_CXT
-from avid.externals.doseTool import saveSimpleDictAsResultXML
+from avid.externals.plastimatch import FORMAT_VALUE_PLM_CXT
 
 logger = logging.getLogger(__name__)
 
-class plmRTSSMap(CLIActionBase):
+class plmRTSSMapAction(CLIActionBase):
   '''Class that wraps the single action for the tool plastimatch convert in order to map DICOM RT SS via a registration.'''
 
   def __init__(self, rtss, reg, outputFormat = artefactProps.FORMAT_VALUE_DCM,
@@ -37,7 +37,7 @@ class plmRTSSMap(CLIActionBase):
                session = None, additionalActionProps = None, actionConfig = None, propInheritanceDict = dict()):
     CLIActionBase.__init__(self, actionTag, alwaysDo, session, additionalActionProps, actionConfig=actionConfig, propInheritanceDict=propInheritanceDict)
     self._addInputArtefacts(rtss=rtss, reg = reg)
-    self._rtts = rtss
+    self._rtss = rtss
     self._reg = reg
     self._outputFormat = outputFormat
 
@@ -50,14 +50,16 @@ class plmRTSSMap(CLIActionBase):
     if not objective is None:
         name += '-%s'%objective
 
-    name += "_#"+str(artefactHelper.getArtefactProperty(self._rtss,artefactProps.TIMEPOINT))\
-            +"_by_"+str(artefactHelper.getArtefactProperty(self._reg,artefactProps.ACTIONTAG))
+    name += "_#"+str(artefactHelper.getArtefactProperty(self._rtss,artefactProps.TIMEPOINT))
 
-    objective = artefactHelper.getArtefactProperty(self._reg,artefactProps.OBJECTIVE)
-    if not objective is None:
-        name += '-%s'%objective
+    if self._reg is not  None:
+      name += "_by_"+str(artefactHelper.getArtefactProperty(self._reg,artefactProps.ACTIONTAG))
 
-    name += "_#"+str(artefactHelper.getArtefactProperty(self._reg,artefactProps.TIMEPOINT))
+      objective = artefactHelper.getArtefactProperty(self._reg,artefactProps.OBJECTIVE)
+      if not objective is None:
+          name += '-%s'%objective
+
+      name += "_#"+str(artefactHelper.getArtefactProperty(self._reg,artefactProps.TIMEPOINT))
 
     return name
 
@@ -87,37 +89,50 @@ class plmRTSSMap(CLIActionBase):
     regFormat = artefactHelper.getArtefactProperty(self._reg,artefactProps.FORMAT)
     resultPath = artefactHelper.getArtefactProperty(self._resultArtefact,artefactProps.URL)
 
-    if regFormat == FORMAT_VALUE_MATCHPOINT:
-      regPath = getDeformationFieldPath(regPath)
-
     execURL = AVIDUrlLocater.getExecutableURL(self._session, "plastimatch", self._actionConfig)
     
-    content = '"' + execURL + '" convert ' + ' --input "' + rtssPath + '"' + ' --xf "' + regPath
+    content = '"' + execURL + '" convert ' + ' --input "' + rtssPath + '"'
+
+    if self._reg is not  None:
+      if regFormat == FORMAT_VALUE_MATCHPOINT:
+        regPath = getDeformationFieldPath(regPath)
+      content += ' --xf "' + regPath + '"'
 
     if self._outputFormat == artefactProps.FORMAT_VALUE_DCM:
-      content = content + ' --output-dicom "'+resultPath+'"'
+      content += ' --output-dicom "'+os.path.splitext(resultPath)[0]+'"'
     elif self._outputFormat == FORMAT_VALUE_PLM_CXT:
-      content = content + ' --output-cxt "'+resultPath+'"'
+      content += ' --output-cxt "'+resultPath+'"'
     else:
       raise ValueError('Output format is not supported by plmRTSSMap action. Choosen format: {}'.format(self._outputFormat))
       
     return content
+
+  def _postProcessCLIExecution(self):
+    if self._outputFormat == artefactProps.FORMAT_VALUE_DCM:
+      resultPath = artefactHelper.getArtefactProperty(self._resultArtefact, artefactProps.URL)
+      dicomDir = os.path.splitext(resultPath)[0]
+
+      for file in os.listdir(dicomDir):
+        copyfile(os.path.join(dicomDir,file), resultPath)
+        break #we assume that plastimatch outputs only on file (the warpped/mapped RT structure set) in the result dir
 
 
 class plmRTSSMapBatchAction(BatchActionBase):
   '''Base class for action objects that are used together with selectors and
     should therefore able to process a batch of SingleActionBased actions.'''
   
-  def __init__(self,  refSelector, testSelector,
-               inputLinker = CaseLinker(), 
-               actionTag = "plmDice", alwaysDo = False,
+  def __init__(self,  rtssSelector, regSelector = None,
+               regLinker = CaseLinker(),
+               actionTag = "plmRTSSMap", alwaysDo = False,
                session = None, additionalActionProps = None, scheduler = SimpleScheduler(), **singleActionParameters):
     BatchActionBase.__init__(self, actionTag, alwaysDo, scheduler, session, additionalActionProps)
 
-    self._refImages = refSelector.getSelection(self._session.artefacts)
-    self._testImages = testSelector.getSelection(self._session.artefacts)
+    self._rttss = rtssSelector.getSelection(self._session.artefacts)
+    self._regs = list()
+    if regSelector is not None:
+      self._regs = regSelector.getSelection(self._session.artefacts)
     
-    self._inputLinker = inputLinker
+    self._regLinker = regLinker
     self._singleActionParameters = singleActionParameters
 
       
@@ -125,17 +140,19 @@ class plmRTSSMapBatchAction(BatchActionBase):
     #filter only type result. Other artefact types are not interesting
     resultSelector = TypeSelector(artefactProps.TYPE_VALUE_RESULT)
     
-    refs = self.ensureRelevantArtefacts(self._refImages, resultSelector, "plm dice 1st input")
-    tests = self.ensureRelevantArtefacts(self._testImages, resultSelector, "plm dice 2nd input")
+    rtssList = self.ensureRelevantArtefacts(self._rttss, resultSelector, "RTSS input")
+    regs = self.ensureRelevantArtefacts(self._regs, resultSelector, "Registrations input")
     
     actions = list()
     
-    for (pos,refImage) in enumerate(refs):
-      linked2 = self._inputLinker.getLinkedSelection(pos,refs,tests)
-           
-      for lt in linked2:
-        action = plmDiceAction(refImage, lt,
-                            self._actionTag, alwaysDo = self._alwaysDo,
+    for (pos,rtss) in enumerate(rtssList):
+      linked = self._regLinker.getLinkedSelection(pos,rtssList,regs)
+      if len(linked) == 0:
+        linked = [None]
+
+      for lr in linked:
+        action = plmRTSSMapAction(rtss, lr,
+                            actionTag=self._actionTag, alwaysDo = self._alwaysDo,
                             session = self._session,
                             additionalActionProps = self._additionalActionProps,
                             **self._singleActionParameters)
---
+++ avid/tests/test_actions_plmRTSSMap.py
@@ -0,0 +1,64 @@
+# AVID
+# Automated workflow system for cohort analysis in radiology and radiation therapy
+#
+# Copyright (c) German Cancer Research Center,
+# Software development for Integrated Diagnostic and Therapy (SIDT).
+# All rights reserved.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.
+#
+# See LICENSE.txt or http://www.dkfz.de/en/sidt/index.html for details.
+
+import unittest
+import os
+import shutil
+import avid.common.workflow as workflow
+from avid.actions.plmRTSSMap import plmRTSSMapBatchAction as plmRTTSMap
+from avid.common.artefact.defaultProps import FORMAT_VALUE_DCM
+from avid.externals.plastimatch import FORMAT_VALUE_PLM_CXT
+from avid.selectors.keyValueSelector import ActionTagSelector
+from avid.common.AVIDUrlLocater import getToolConfigPath
+
+@unittest.skipIf(getToolConfigPath('Plastimatch') is None, 'Tool Plastimatch not installed on the system.')
+class TestPlmRTSSMap(unittest.TestCase):
+
+    def setUp(self):
+      self.testRTSS = os.path.join(os.path.split(__file__)[0],"data", "voxelizerTest", "rtss.dcm")
+      self.sessionDir = os.path.join(os.path.split(__file__)[0],"temporary_test_plmRTSSMap")
+      self.testArtefactFile = os.path.join(os.path.split(__file__)[0],"data", "voxelizerTest", "testlist.avid")
+
+      self.session = workflow.initSession(os.path.join(self.sessionDir, "test.avid"),
+                                          expandPaths=True, bootstrapArtefacts=self.testArtefactFile)
+
+    def tearDown(self):
+      try:
+        shutil.rmtree(self.sessionDir)
+      except:
+        pass
+
+    def test_simple_action(self):
+      
+      action = plmRTTSMap(ActionTagSelector('Struct'), outputFormat = FORMAT_VALUE_DCM,
+                          actionTag = "TestPlmRTSSMap")
+      token = action.do()
+                    
+      self.assertEqual(token.isSuccess(), True)
+
+      token = action.do()
+      self.assertEqual(token.isSkipped(), True)
+
+
+    def test_action_cxt(self):
+        action = plmRTTSMap(ActionTagSelector('Struct'), outputFormat=FORMAT_VALUE_PLM_CXT,
+                            actionTag="TestPlmRTSSMap")
+        token = action.do()
+
+        self.assertEqual(token.isSuccess(), True)
+
+    #TODO ad test that checks mapping with reg
+    #def test_action_with_reg(self):
+
+if __name__ == "__main__":
+    unittest.main()
