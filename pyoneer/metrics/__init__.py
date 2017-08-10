MIC-DKFZ/AVID

commit 2566d5ebc2c62b88762840850dde89fb1bceb13c

better handling of complete failed workflows
Signed-off-by: Ralf Floca <r.floca@dkfz-heidelberg.de>

pyoneer/metrics/__init__.py
@@ -92,9 +92,17 @@ class DefaultMetric (object):
     
     evaluator = DataSetEvaluator(self._metricCriteria, self._instanceDefiningProps, self._instanceDefiningSelector)
     
-    artefacts = loadArtefactList_xml(sessionFile, True, self.sessionDir)
+    artefacts = None
+
+    gmeasure = dict()
+    imeasure = dict()
+    try:
+      loadArtefactList_xml(sessionFile, True, self.sessionDir)
+      gmeasure, imeasure = evaluator.evaluate(artefacts)
+    except:
+      logger.debug('Error. Cannot load artefacts. Evaluation candidate workflow might have failed.')
     
-    gmeasure, imeasure = evaluator.evaluate(artefacts)
+
        
     if self._clearSessionDir:
       artefactDir = os.path.join(self.sessionDir, sessionName)

commit b79b485d0e6ca67f275e8284d92494abbe06d1f2

fixed a bug introduced by 2566d5ebc2c62b88762840850dde89fb1bceb13c
Signed-off-by: Ralf Floca <r.floca@dkfz-heidelberg.de>

pyoneer/metrics/__init__.py
@@ -97,7 +97,7 @@ class DefaultMetric (object):
     gmeasure = dict()
     imeasure = dict()
     try:
-      loadArtefactList_xml(sessionFile, True, self.sessionDir)
+      artefacts = loadArtefactList_xml(sessionFile, True, self.sessionDir)
       gmeasure, imeasure = evaluator.evaluate(artefacts)
     except:
       logger.debug('Error. Cannot load artefacts. Evaluation candidate workflow might have failed.')