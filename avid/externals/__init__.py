a7fe930060453a25f9982e7939b415fcefa8b599
Ralf Floca <r.floca@dkfz-heidelberg.de>
2018-07-24 14:39:18 +0200

    Added cmake like configExternalFile function
    Helper function to modify parameter files i.a. of external tools.
    Simelar to CMake's config_file
    
    Signed-off-by: Ralf Floca <r.floca@dkfz-heidelberg.de>

--- avid/externals/__init__.py
+++ avid/externals/__init__.py
@@ -10,3 +10,25 @@
 # A PARTICULAR PURPOSE.
 #
 # See LICENSE.txt or http://www.dkfz.de/en/sidt/index.html for details.
+
+import os
+from avid.common.osChecker import checkAndCreateDir
+
+
+def configureExternalFile(inFilePath, outFilePath, paramDict, paramEscape='@'):
+    '''reads a given inFilePath configures/modifies it by the given parameter dictionary
+    and stores the result at outFilePath.
+    Configuration is done be searching for any key in the paramDict and replacing its
+    occurence with the value associated with the key.
+    @param paramEscape specifies how the keys are escaped in the input file. Default would be
+     '@key1@'.'''
+    with open(inFilePath, 'r') as ifile_handle:
+        content = ifile_handle.read()
+
+        for key in paramDict:
+            symbol = paramEscape + str(key) + paramEscape
+            content = content.replace(symbol, str(paramDict[key]))
+
+        checkAndCreateDir(os.path.split(outFilePath)[0])
+        with open(outFilePath, 'w') as ofile_handle:
+            ofile_handle.write(content)
