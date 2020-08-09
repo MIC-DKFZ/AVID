MIC-DKFZ/AVID

commit d0d44234a22c8838fb0b94a31004f2e707f5e67d

Cherry picked commit bf3542c
Signed-off-by: Clemens Hentschke <c.hentschke@dkfz-heidelberg.de>

avid/actions/mapR.py
@@ -33,7 +33,7 @@ class mapRAction(CLIActionBase):
 
   def __init__(self, inputImage, registration = None, templateImage = None, 
                interpolator = "linear", outputExt = "nrrd", paddingValue = None,
-               actionTag = "mapR", alwaysDo = False,
+               actionTag = "mapR", inputIsReference = True, alwaysDo = False,
                session = None, additionalActionProps = None, actionConfig = None, propInheritanceDict = None):
     CLIActionBase.__init__(self, actionTag, alwaysDo, session, additionalActionProps, actionConfig = actionConfig, propInheritanceDict = propInheritanceDict)
     self._addInputArtefacts(inputImage=inputImage, registration=registration, templateImage=templateImage)
@@ -43,6 +43,7 @@ class mapRAction(CLIActionBase):
     self._interpolator = interpolator
     self._outputExt = outputExt
     self._paddingValue = paddingValue
+    self._inputIsReference = inputIsReference
     
     cwd = os.path.dirname(AVIDUrlLocater.getExecutableURL(self._session, "mapR", actionConfig))
     self._cwd = cwd    
@@ -125,7 +126,7 @@ class mapRBatchAction(BatchActionBase):
   
   def __init__(self,  inputSelector, registrationSelector = None, templateSelector = None,
                regLinker = FractionLinker(), templateLinker = CaseLinker(), 
-               actionTag = "mapR", alwaysDo = False,
+               actionTag = "mapR", inputIsReference=True, alwaysDo = False,
                session = None, additionalActionProps = None, scheduler = SimpleScheduler(), templateRegLinker = LinkerBase(),
                **singleActionParameters):
     '''@param inputSelector Selector for the artefacts that should be mapped.
@@ -152,6 +153,8 @@ class mapRBatchAction(BatchActionBase):
     self._regLinker = regLinker
     self._templateLinker = templateLinker
     self._templateRegLinker = templateRegLinker
+
+    self._inputIsReference = inputIsReference
 
     self._singleActionParameters = singleActionParameters
 
@@ -183,6 +186,7 @@ class mapRBatchAction(BatchActionBase):
                               actionTag = self._actionTag,
                               alwaysDo = self._alwaysDo,
                               session = self._session,
+                              inputIsReference=self._inputIsReference,
                               additionalActionProps = self._additionalActionProps,
                               **self._singleActionParameters)
           actions.append(action)

commit 42f94f5dcb53d0143631ed4711622b7808aeeea4

Fixed error introduced by d0d44234a22c
inputIsReference is not working correctly
Added test to check this feature.

avid/actions/mapR.py
@@ -70,7 +70,15 @@ class mapRAction(CLIActionBase):
     if self._hasDICOMinput():
       userDefinedProps[artefactProps.FORMAT] = artefactProps.FORMAT_VALUE_DCM
 
-    self._resultArtefact = self.generateArtefact(self._inputImage,
+    artefactRef = self._inputImage
+    if self._inputIsReference is False:
+      if self._templateImage is None:
+        logger.error("template image is None and can't be used as Reference")
+        raise
+      else:
+        artefactRef = self._templateImage
+
+    self._resultArtefact = self.generateArtefact(artefactRef,
                                                  userDefinedProps = userDefinedProps,
                                                  urlHumanPrefix=self.instanceName,
                                                  urlExtension=self._outputExt)

avid/tests/data/mapRTest/testlist_2.avid
@@ -17,23 +17,7 @@
 	</avid:artefact>
 	<avid:artefact>
 		<avid:property key="case">case1</avid:property>
-		<avid:property key="timePoint">2</avid:property>
-		<avid:property key="actionTag">Moving</avid:property>
-		<avid:property key="type">result</avid:property>
-		<avid:property key="format">itk</avid:property>
-		<avid:property key="url">./MatchPointLogoShifted10x-16y-5z.mhd</avid:property>
-	</avid:artefact>
-	<avid:artefact>
-		<avid:property key="case">case1</avid:property>
 		<avid:property key="timePoint">1</avid:property>
-		<avid:property key="actionTag">Registration</avid:property>
-		<avid:property key="type">result</avid:property>
-		<avid:property key="format">MatchPoint</avid:property>
-		<avid:property key="url">./Identity.mapr</avid:property>
-	</avid:artefact>
-	<avid:artefact>
-		<avid:property key="case">case1</avid:property>
-		<avid:property key="timePoint">2</avid:property>
 		<avid:property key="actionTag">Registration</avid:property>
 		<avid:property key="type">result</avid:property>
 		<avid:property key="format">MatchPoint</avid:property>

avid/tests/test_actions_mapR.py
@@ -16,6 +16,7 @@ import os
 import shutil
 import avid.common.workflow as workflow
 from avid.actions.mapR import mapRBatchAction as mapR
+from avid.common.artefact.defaultProps import TIMEPOINT
 from avid.selectors.keyValueSelector import ActionTagSelector
 
 from avid.common.AVIDUrlLocater import getToolConfigPath
@@ -28,7 +29,8 @@ class TestMapR(unittest.TestCase):
       self.testDataDir = os.path.join(os.path.split(__file__)[0],"data", "mapRTest")
       self.testArtefactFile = os.path.join(os.path.split(__file__)[0],"data", "mapRTest", "testlist.avid")
       self.sessionDir = os.path.join(os.path.split(__file__)[0],"temporary_test_mapr")
-      
+      self.testArtefactFile2 = os.path.join(os.path.split(__file__)[0],"data", "mapRTest", "testlist_2.avid")
+
       self.session = workflow.initSession(os.path.join(self.sessionDir, "test.avid"), expandPaths=True, bootstrapArtefacts=self.testArtefactFile)
 
               
@@ -58,6 +60,25 @@ class TestMapR(unittest.TestCase):
 
       token = action.do()
       self.assertEqual(token.isSuccess(), True)
+
+    def test_mapr_action_inputIsReference(self):
+
+        newsession = workflow.initSession(os.path.join(self.sessionDir, "testlist_2.avid"), expandPaths=True,
+                                            bootstrapArtefacts=self.testArtefactFile2)
+
+        action = mapR(ActionTagSelector("Moving"), ActionTagSelector("Registration"), ActionTagSelector("Target"),
+                      alwaysDo=True, actionTag="TestMapping")
+        token = action.do()
+        self.assertEqual(token.isSuccess(), True)
+        self.assertEqual(token.generatedArtefacts[0][TIMEPOINT], 1)
+
+        action = mapR(ActionTagSelector("Moving"), ActionTagSelector("Registration"), ActionTagSelector("Target"),
+                      inputIsReference=False, alwaysDo=True, actionTag="TestMapping")
+        token = action.do()
+        self.assertEqual(token.isSuccess(), True)
+        #now the template should be reference for output artefacts, thus the time point should be 0 (timpoint of
+        #the template
+        self.assertEqual(token.generatedArtefacts[0][TIMEPOINT], 0)
 
 
 if __name__ == "__main__":