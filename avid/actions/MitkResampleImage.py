487d4ed3a1322e52b4a26aa814e70dbc82ca27b5
Ralf Floca <r.floca@dkfz-heidelberg.de>
2021-04-19 15:52:58 +0200

    Refactored MitkResampleImage to use GenericCLIAction

--- avid/actions/MitkResampleImage.py
+++ avid/actions/MitkResampleImage.py
@@ -25,69 +25,25 @@ from avid.linkers import FractionLinker
 from avid.splitter import BaseSplitter, KeyValueSplitter
 
 from . import BatchActionBase
-from .cliActionBase import CLIActionBase
+from .genericCLIAction import GenericCLIAction
 from avid.selectors import TypeSelector
 from .simpleScheduler import SimpleScheduler
 
 logger = logging.getLogger(__name__)
 
-class MitkResampleImageAction(CLIActionBase):
+class MitkResampleImageAction(GenericCLIAction):
     '''Class that wraps the single action for the tool MITKResampleImage.'''
-
-    def __init__(self, images, cliArgs= None, actionTag="MitkResampleImage", alwaysDo=False,
-                 session=None, additionalActionProps=None, actionConfig=None, propInheritanceDict=None):
-        CLIActionBase.__init__(self, actionTag, alwaysDo, session, additionalActionProps,
-                               actionID="MitkCLGlobalImageFeatures", actionConfig=actionConfig,
-                               propInheritanceDict=propInheritanceDict)
-
-        self._addInputArtefacts(images=images)
-        self._images = self._ensureSingleArtefact(images, "images")
-
-        self._cliArgs = dict()
-        if cliArgs is not None:
-            illegalArgs = ["i", "image", "o", "output"]
-            for arg in cliArgs:
-                if arg not in illegalArgs:
-                    self._cliArgs[arg] = cliArgs[arg]
-                else:
-                    logger.warning('Ignored illegal argument "{}". It will be set by action'.format(arg))
-
-    def _generateName(self):
-        name = "res_{}".format(artefactHelper.getArtefactShortName(self._images))
-        return name
-
-    def _indicateOutputs(self):
-        # Specify result artefact
-        self._resultArtefact = self.generateArtefact(self._images,
-                                                     userDefinedProps={artefactProps.TYPE: artefactProps.TYPE_VALUE_RESULT,
-                                                                       artefactProps.FORMAT: artefactProps.FORMAT_VALUE_ITK},
-                                                     urlHumanPrefix=self.instanceName, urlExtension='nrrd')
-        return [self._resultArtefact]
-
-    def _prepareCLIExecution(self):
-
-        resultPath = artefactHelper.getArtefactProperty(self._resultArtefact, artefactProps.URL)
-        imagePath = artefactHelper.getArtefactProperty(self._images, artefactProps.URL)
-
-        osChecker.checkAndCreateDir(os.path.split(resultPath)[0])
-
-        try:
-            execURL = AVIDUrlLocater.getExecutableURL(self._session, self._actionID, self._actionConfig)
-
-            content = '"{}" -i "{}" -o "{}"'.format(execURL, imagePath, resultPath)
-            for arg in self._cliArgs:
-                content += ' -{}'.format(arg)
-                if self._cliArgs[arg] is not None:
-                    content += ' {}'.format(self._cliArgs[arg])
-        except:
-            logger.error("Error for getExecutable.")
-            raise
-
-        return content
+    def __init__(self, images, cliArgs= None, defaultoutputextension = 'nrrd', actionTag="MitkResampleImage",
+                 alwaysDo=False, session=None, additionalActionProps=None, actionConfig=None, propInheritanceDict=None):
+        GenericCLIAction.__init__(self, i=images, actionID="MitkResampleImage", outputFlags=['o'],
+                                  cliArgs=cliArgs, illegalArgs= ['output','input'], actionTag= actionTag,
+                                  alwaysDo=alwaysDo, session=session, additionalActionProps=additionalActionProps,
+                                  actionConfig=actionConfig, propInheritanceDict=propInheritanceDict,
+                                  defaultoutputextension=defaultoutputextension)
 
 
 class MitkResampleImageBatchAction(BatchActionBase):
-    '''Batch action for MITKCLGlobalImageFeatures to produce XML results.'''
+    '''Batch action for MitkResampleImage to produce XML results.'''
 
     def __init__(self, imageSelector,
                  actionTag="MITKResampleImage", alwaysDo=False, session=None,
