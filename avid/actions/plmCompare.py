c75cfb0b01fd6523ab4f2fbf6a82d84a2d64a514
Stephen Schaumann <s.schaumann@dkfz-heidelberg.de>
2024-02-21 14:29:34 +0100

    update copyright header to Apache 2.0 with SPDX

--- avid/actions/plmCompare.py
+++ avid/actions/plmCompare.py
@@ -1,15 +1,20 @@
-# AVID
-# Automated workflow system for cohort analysis in radiology and radiation therapy
+# SPDX-FileCopyrightText: 2024, German Cancer Research Center (DKFZ), Division of Medical Image Computing (MIC)
 #
-# Copyright (c) German Cancer Research Center,
-# Software development for Integrated Diagnostic and Therapy (SIDT).
-# All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 #
-# This software is distributed WITHOUT ANY WARRANTY; without
-# even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# See LICENSE.txt or http://www.dkfz.de/en/sidt/index.html for details.
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or find it in LICENSE.txt.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 import logging
